a0bfd5651f7a92b3938c5eccc00843ff7a79dc7b
Sebastian Ramadan <plebbyastian@gmail.com>
2018-06-07 16:02:29 +1000

    Title scraping

--- irc.js
+++ irc.js
@@ -1,34 +1,46 @@
 var irc = (function(socket, nickname, password, channels)
-{ var web = require('http');
+{ var dom = require('xmldom').DOMParser;
+  var fetch = require('node-fetch');
+  var url = require('url-regex')();
   var data;
+
+  function urbandict(match) { fetch('http://api.urbandictionary.com/v0/define?term=' + encodeURIComponent(match[3])).then(res => res.json()).then(res => { var list = res['list'];
+                                                                                                                                                           var x = Number(match[2] || 0);
+                                                                                                                                                           if (x >= 0 && x < list.length) { socket.write('PRIVMSG ' + match[1] + ' :' + match[3] + ' [' + x + '/' + list.length + ']: ' + list[x]['definition'] + '\r\n'); }
+                                                                                                                                                           else { socket.write('PRIVMSG ' + match[1] + ' :I see your request for ' + match[3] + ' [' + x + '/' + list.length + '] and I don\'t like it.\r\n'); }
+                                                                                                                                                         });
+                            }
+
+  function urlscrape(match) { var m = match[2].match(url);
+                              if (m === null) { return; }
+                              m.forEach(function(u){ fetch(u).then(res => res.text()).then(res => { var title = (new dom()).parseFromString(res).getElementsByTagName('title');
+                                                                                                    if (title.length == 0) { return; }
+                                                                                                    socket.write('PRIVMSG ' + match[1] + ' :<' + u + '> -- ' + title[0].childNodes + '\r\n');
+                                                                                                  });
+                                                   });
+                            } 
+
+  var h = [ { 'pattern': /^PING( .*)?$/
+            , 'handler': (match) => { socket.write('PONG' + match[0] + '\r\n'); }
+            }
+          , { 'pattern': /^\:[^ ]+ 001/
+            , 'handler': (match) => { socket.write('JOIN ' + channels + '\r\n'); }
+            }
+          , { 'pattern': /^\:[^ ]+ 437 [^ ]+ ([^ ]+)/
+            , 'handler': (match) => { socket.write('NICK ' + match[0] + '`\r\n'); }
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? PRIVMSG (\#[^ ]+) \:\.ud (?:(\d+) )?(.*)$/
+            , 'handler': urbandict
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE) (\#[^ ]+) \:(.*)$/
+            , 'handler': urlscrape
+            }
+          ];
+
   socket.on('connect', () => { socket.write((password && 'PASS ' + password + '\r\n') +
                                             'NICK ' + nickname + '\r\n' +
                                             'USER ' + nickname + ' 0 * :' + nickname + '\r\n'); });
-  socket.on('data', (part) => { var h = [ { 'pattern': /^PING( .*)?$/
-                                          , 'handler': (match) => { socket.write('PONG' + match[0] + '\r\n'); }
-                                          }
-                                        , { 'pattern': /^\:[^ ]+ 001/
-                                          , 'handler': (match) => { socket.write('JOIN ' + channels + '\r\n'); }
-                                          }
-                                        , { 'pattern': /^\:[^ ]+ 437 [^ ]+ ([^ ]+)/
-                                          , 'handler': (match) => { socket.write('NICK ' + match[0] + '`\r\n'); }
-                                          }
-                                        , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? PRIVMSG (\#[^ ]+) \:\.ud (?:(\d+) )?(.*)$/
-                                          , 'handler': (match) =>
-                                                       { (function(){ var json = '';
-                                                                      web.get('http://api.urbandictionary.com/v0/define?term=' + encodeURIComponent(match[3]), (resp) =>
-                                                                                                                                                               { resp.on('data', (chunk) => { json += chunk; });
-                                                                                                                                                                 resp.on('end', () => { var list = JSON.parse(json)['list'];
-                                                                                                                                                                                        var x = Number(match[2] || 0);
-                                                                                                                                                                                        if (x >= 0 && x < list.length) { socket.write('PRIVMSG ' + match[1] + ' :' + match[3] + ' [' + x + '/' + list.length + ']: ' + list[x]['definition'] + '\r\n'); }
-                                                                                                                                                                                        else { socket.write('PRIVMSG ' + match[1] + ' :I see your request for ' + match[3] + ' [' + x + '/' + list.length + '] and I don\'t like it.\r\n'); }
-                                                                                                                                                                                      });
-                                                                                                                                                               }).on('error', () => { socket.write('PRIVMSG ' + match[1] + ' :I see your request for ' + match[3] + ' and I don\'t like it.\r\n'); });
-                                                                    })();
-                                                       }
-                                          }
-                                        ];
-                                data += part;
+  socket.on('data', (part) => { data += part;
                                 var head = 0, newline = /[\r\n]/g;
                                 while (newline.exec(data) !== null)
                                 { var line = data.substring(head, newline.lastIndex - 1);
@@ -40,8 +52,11 @@ var irc = (function(socket, nickname, password, channels)
                                 }
                                 data = data.substring(head);
                               });
+
   return socket;
 });
 
-var sock = irc(require('tls').connect({ 'host': 'irc.freenode.net'
-                                      , 'port': '7000' }), 'Sebivor`', '', '##newboston');
+irc(require('tls').connect({ 'host': 'irc.freenode.net'
+                           , 'port': '7000' }), 'Sebibot', '', '##newboston');
+irc(require('net').connect({ 'host': 'irc.librairc.net'
+                           , 'port': '6667' }), 'Sebibot', '', '#potty');
