86e2b39bf77e303e445d11b137140e098f337c49
Sebastian Ramadan <plebbyastian@gmail.com>
2018-06-08 20:47:10 +1000

    Fix newline-related bug

--- irc.js
+++ irc.js
@@ -1,13 +1,17 @@
-var irc = (function(socket, nickname, password, channels)
+var irc = (function(socket, nickname, password, channel)
 { var dom = require('xmldom').DOMParser;
   var fetch = require('node-fetch');
   var url = require('url-regex')();
   var data;
 
+  function msg(socket, destination, message) { var cmd = 'PRIVMSG ' + destination + ' :' + message;
+                                               socket.write(cmd.substr(0,510).replace(/[\r\n]/g, ' ').replace(/ +/g,' ') + '\r\n');
+                                             }
+
   function urbandict(match) { fetch('http://api.urbandictionary.com/v0/define?term=' + encodeURIComponent(match[3])).then(res => res.json()).then(res => { var list = res['list'];
                                                                                                                                                            var x = Number(match[2] || 0);
-                                                                                                                                                           if (x >= 0 && x < list.length) { socket.write('PRIVMSG ' + match[1] + ' :' + match[3] + ' [' + x + '/' + list.length + ']: ' + list[x]['definition'] + '\r\n'); }
-                                                                                                                                                           else { socket.write('PRIVMSG ' + match[1] + ' :I see your request for ' + match[3] + ' [' + x + '/' + list.length + '] and I don\'t like it.\r\n'); }
+                                                                                                                                                           if (x >= 0 && x < list.length) { msg(socket, match[1], match[3] + ' [' + x + '/' + list.length + ']: ' + list[x]['definition']); }
+                                                                                                                                                           else { msg(socket, match[1], 'I see your request for ' + match[3] + ' and I don\'t like it.'); }
                                                                                                                                                          });
                             }
 
@@ -15,7 +19,7 @@ var irc = (function(socket, nickname, password, channels)
                               if (m === null) { return; }
                               m.forEach(function(u){ fetch(u).then(res => res.text()).then(res => { var title = (new dom()).parseFromString(res).getElementsByTagName('title');
                                                                                                     if (title.length == 0) { return; }
-                                                                                                    socket.write('PRIVMSG ' + match[1] + ' :<' + u + '> -- ' + title[0].childNodes + '\r\n');
+                                                                                                    msg(socket, match[1], '<' + u + '> -- ' + title[0].childNodes);
                                                                                                   });
                                                    });
                             } 
@@ -24,13 +28,41 @@ var irc = (function(socket, nickname, password, channels)
             , 'handler': (match) => { socket.write('PONG' + match[0] + '\r\n'); }
             }
           , { 'pattern': /^\:[^ ]+ 001/
-            , 'handler': (match) => { socket.write('JOIN ' + channels + '\r\n'); }
+            , 'handler': (match) => { socket.write('JOIN ' + channel + '\r\n' +
+                                                   'MODE ' + channel + '+s-n\r\n'); }
             }
           , { 'pattern': /^\:[^ ]+ 437 [^ ]+ ([^ ]+)/
             , 'handler': (match) => { socket.write('NICK ' + match[0] + '`\r\n'); }
             }
-          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? PRIVMSG (\#[^ ]+) \:\.ud (?:(\d+) )?(.*)$/
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE|332 [^ ]+) (\#[^ ]+) \:\.nick (.*)$/
+            , 'handler': (match) => { if (match[1] != channel) { return; }
+                                      socket.write('NICK ' + match[2] + '\r\n'); }
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE|332 [^ ]+) (\#[^ ]+) \:\.join (.*)$/
+            , 'handler': (match) => { if (match[1] != channel) { return; }
+                                      socket.write('JOIN ' + match[2] + '\r\n'); }
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE|332 [^ ]+) (\#[^ ]+) \:\.part (.*)$/
+            , 'handler': (match) => { if (match[1] != channel) { return; }
+                                      socket.write('PART ' + match[2] + '\r\n'); }
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE|332 [^ ]+) (\#[^ ]+) \:\.msg ([^ ]+) (.*)$/
+            , 'handler': (match) => { if (match[1] != channel) { return; }
+                                      socket.write('PRIVMSG ' + match[2] + ' :' + match[3] + '\r\n'); }
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE|332 [^ ]+) (\#[^ ]+) \:\.note ([^ ]+) (.*)$/
+            , 'handler': (match) => { if (match[1] != channel) { return; }
+                                      socket.write('NOTICE ' + match[2] + ' :' + match[3] + '\r\n'); }
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE|332 [^ ]+) (\#[^ ]+) \:\.mode ([^ ]+) (.*)$/
+            , 'handler': (match) => { if (match[1] != channel) { return; }
+                                      socket.write('MODE ' + match[2] + ' ' + match[3] + '\r\n'); }
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE) (\#[^ ]+) \:\.ud (?:(\d+) )?(.*)$/
             , 'handler': urbandict
+            }
+          , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE) (\#[^ ]+) \:\.(src|source|github)$/
+            , 'handler': (match) => { sockwrite('PRIVMSG ' + match[1] + ' :My source code can be found at https://github.com/Sebbyastian/newbotonian/blob/master/irc.js\r\n'); }
             }
           , { 'pattern': /^\:([^ !]+)(?:\![^ ]+)? (?:PRIVMSG|NOTICE) (\#[^ ]+) \:(.*)$/
             , 'handler': urlscrape
@@ -57,6 +89,6 @@ var irc = (function(socket, nickname, password, channels)
 });
 
 irc(require('tls').connect({ 'host': 'irc.freenode.net'
-                           , 'port': '7000' }), 'Sebibot', '', '##newboston');
+                           , 'port': '7000' }), 'Sebibot', '', '#secret');
 irc(require('net').connect({ 'host': 'irc.librairc.net'
-                           , 'port': '6667' }), 'Sebibot', '', '#potty');
+                           , 'port': '6667' }), 'Sebibot', '', '#secret');
